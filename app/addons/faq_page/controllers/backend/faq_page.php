d522dee62411aea9d40bce1c955cb257b2ac2879
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-23 15:38:36 +0400

    UNIT-7 : Add answer variable for testing view

--- app/addons/faq_page/controllers/backend/faq_page.php
+++ app/addons/faq_page/controllers/backend/faq_page.php
@@ -11,13 +11,12 @@ if ($mode == 'manage') {
 // Registry::get('settings.Appearance.admin_elements_per_page'));
 
     $question = 'Whazaap?';
+    $answer = "It's alright dog";
 
-    // Tygh::$app['view']->assign(
-    //     'banners'  => $banners,
-    //     'search' => $params,
-    // ));
+    Tygh::$app['view']->assign([
+        'question'  => $question,
+        'answer' => $answer,
+    ]);
 
-    Tygh::$app['view']->assign(
-        'question' => $question,
-    );
+    // Tygh::$app['view']->assign('question', $question);
 }
