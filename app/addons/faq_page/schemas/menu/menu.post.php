b9ccaead8fcd120ebeed83547473fd61aa65aac4
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-23 10:06:19 +0400

    UNIT-7 : Add addon link to the marketing menu of admin panel

---
+++ app/addons/faq_page/schemas/menu/menu.post.php
@@ -0,0 +1,11 @@
+<?php
+
+$schema['central']['marketing']['items']['faq_page'] = [
+    'attrs' => [
+        'class'=>'is-addon'
+    ],
+    'href' => 'faq_page.manage',
+    'position' => 900
+];
+
+return $schema;
