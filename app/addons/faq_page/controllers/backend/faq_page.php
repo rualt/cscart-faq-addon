3f79bafcd4ec390e56a85e800f77dacc70dd744f
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-24 15:54:10 +0400

    UNIT-7 : Add 3 test question arrays

--- app/addons/faq_page/controllers/backend/faq_page.php
+++ app/addons/faq_page/controllers/backend/faq_page.php
@@ -10,11 +10,36 @@ if ($mode == 'manage') {
 // list($banners, $params) = fn_get_banners($_REQUEST, DESCR_SL,
 // Registry::get('settings.Appearance.admin_elements_per_page'));
 
-    $question = 'Whazaap?';
+    $questions = [
+        [   
+            'question_id' => 1,
+            'question' => "question 1?",
+            'answer' => 'answer 1',
+            'author' => 'John Snow',
+            'position' => 20,
+            'status' => "A",
+        ],
+        [   
+            'question_id' => 2,
+            'question' => "question 2?",
+            'answer' => 'answer 2',
+            'author' => 'Tirion Lannister',
+            'position' => 10,
+            'status' => "A"
+        ],
+        [   
+            'question_id' => 3,
+            'question' => "question 3?",
+            'answer' => 'answer 3',
+            'author' => 'Aria Stark',
+            'position' => 30,
+            'status' => "A"
+        ]
+    ];
     $answer = "It's alright dog";
 
     Tygh::$app['view']->assign([
-        'question'  => $question,
+        'questions'  => $questions,
         'answer' => $answer,
     ]);
 
