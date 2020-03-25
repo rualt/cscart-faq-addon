a47caac2950b644527e52704378b2a828a9a1ddf
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-25 15:12:18 +0400

    UNIT-7 : Add prefix 'faq_' to addon db

--- app/addons/faq_page/func.php
+++ app/addons/faq_page/func.php
@@ -25,10 +25,10 @@ function fn_get_questions($params = array(), $lang_code = CART_LANGUAGE, $items_
 
 
     $sortings = array(
-        'position' => '?:questions.position',
-        'timestamp' => '?:questions.timestamp',
-        'name' => '?:question_descriptions.question',
-        'status' => '?:questions.status',
+        'position' => '?:faq_questions.position',
+        'timestamp' => '?:faq_questions.timestamp',
+        'name' => '?:faq_question_descriptions.question',
+        'status' => '?:faq_questions.status',
     );
     // fn_print_die($sortings);
 
@@ -42,48 +42,48 @@ function fn_get_questions($params = array(), $lang_code = CART_LANGUAGE, $items_
     $sorting = db_sort($params, $sortings, 'name', 'asc');
     // fn_print_die($sorting);
 
-    // $condition .= fn_get_localizations_condition('?:questions.localization');
-    // $condition .= (AREA == 'A') ? '' : db_quote(' AND (?:questions.type != ?s OR ?:question_images.question_image_id IS NOT NULL)', 'G');
+    // $condition .= fn_get_localizations_condition('?:faq_questions.localization');
+    // $condition .= (AREA == 'A') ? '' : db_quote(' AND (?:faq_questions.type != ?s OR ?:question_images.question_image_id IS NOT NULL)', 'G');
 
     if (!empty($params['item_ids'])) {
-        $condition .= db_quote(' AND ?:questions.question_id IN (?n)', explode(',', $params['item_ids']));
+        $condition .= db_quote(' AND ?:faq_questions.question_id IN (?n)', explode(',', $params['item_ids']));
     }
 
     if (!empty($params['name'])) {
-        $condition .= db_quote(' AND ?:question_descriptions.question LIKE ?l', '%' . trim($params['name']) . '%');
+        $condition .= db_quote(' AND ?:faq_question_descriptions.question LIKE ?l', '%' . trim($params['name']) . '%');
     }
 
     if (!empty($params['status'])) {
-        $condition .= db_quote(' AND ?:questions.status = ?s', $params['status']);
+        $condition .= db_quote(' AND ?:faq_questions.status = ?s', $params['status']);
     }
 
     if (!empty($params['period']) && $params['period'] != 'A') {
         list($params['time_from'], $params['time_to']) = fn_create_periods($params);
-        $condition .= db_quote(' AND (?:questions.timestamp >= ?i AND ?:questions.timestamp <= ?i)', $params['time_from'], $params['time_to']);
+        $condition .= db_quote(' AND (?:faq_questions.timestamp >= ?i AND ?:faq_questions.timestamp <= ?i)', $params['time_from'], $params['time_to']);
     }
 
     $fields = array (
-        '?:questions.question_id',
-        '?:questions.status',
-        '?:questions.position',
-        '?:question_descriptions.question',
-        '?:question_descriptions.answer',
-        '?:question_descriptions.author',
+        '?:faq_questions.question_id',
+        '?:faq_questions.status',
+        '?:faq_questions.position',
+        '?:faq_question_descriptions.question',
+        '?:faq_question_descriptions.answer',
+        '?:faq_question_descriptions.author',
     );
 
     // if (fn_allowed_for('ULTIMATE')) {
-    //     $fields[] = '?:questions.company_id';
+    //     $fields[] = '?:faq_questions.company_id';
     // }
 
-    $join .= db_quote(' LEFT JOIN ?:question_descriptions ON ?:question_descriptions.question_id = ?:questions.question_id AND ?:question_descriptions.lang_code = ?s', $lang_code);
+    $join .= db_quote(' LEFT JOIN ?:faq_question_descriptions ON ?:faq_question_descriptions.question_id = ?:faq_questions.question_id AND ?:faq_question_descriptions.lang_code = ?s', $lang_code);
 
     if (!empty($params['items_per_page'])) {
-        $params['total_items'] = db_get_field("SELECT COUNT(*) FROM ?:questions $join WHERE 1 $condition");
+        $params['total_items'] = db_get_field("SELECT COUNT(*) FROM ?:faq_questions $join WHERE 1 $condition");
         $limit = db_paginate($params['page'], $params['items_per_page'], $params['total_items']);
     }
 
     $questions = db_get_hash_array(
-        "SELECT ?p FROM ?:questions " .
+        "SELECT ?p FROM ?:faq_questions " .
         $join .
         "WHERE 1 ?p ?p ?p",
         'question_id', implode(', ', $fields), $condition, $sorting, $limit
@@ -112,25 +112,25 @@ function fn_get_question_data($question_id, $lang_code = CART_LANGUAGE)
     $condition = '';
 
     $fields = array (
-        '?:questions.question_id',
-        '?:questions.status',
-        '?:question_descriptions.question',
-        '?:questions.localization',
-        '?:questions.timestamp',
-        '?:questions.position',
-        '?:question_descriptions.author',
-        '?:question_descriptions.answer',
+        '?:faq_questions.question_id',
+        '?:faq_questions.status',
+        '?:faq_question_descriptions.question',
+        '?:faq_questions.localization',
+        '?:faq_questions.timestamp',
+        '?:faq_questions.position',
+        '?:faq_question_descriptions.author',
+        '?:faq_question_descriptions.answer',
     );
 
     if (fn_allowed_for('ULTIMATE')) {
-        $fields[] = '?:questions.company_id as company_id';
+        $fields[] = '?:faq_questions.company_id as company_id';
     }
 
-    $joins[] = db_quote("LEFT JOIN ?:question_descriptions ON ?:question_descriptions.question_id = ?:questions.question_id AND ?:question_descriptions.lang_code = ?s", $lang_code);
-    // $joins[] = db_quote("LEFT JOIN ?:question_images ON ?:question_images.question_id = ?:questions.question_id AND ?:question_images.lang_code = ?s", $lang_code);
+    $joins[] = db_quote("LEFT JOIN ?:faq_question_descriptions ON ?:faq_question_descriptions.question_id = ?:faq_questions.question_id AND ?:faq_question_descriptions.lang_code = ?s", $lang_code);
+    // $joins[] = db_quote("LEFT JOIN ?:question_images ON ?:question_images.question_id = ?:faq_questions.question_id AND ?:question_images.lang_code = ?s", $lang_code);
 
-    $condition = db_quote("WHERE ?:questions.question_id = ?i", $question_id);
-    $condition .= (AREA == 'A') ? '' : " AND ?:questions.status IN ('A', 'H') ";
+    $condition = db_quote("WHERE ?:faq_questions.question_id = ?i", $question_id);
+    $condition .= (AREA == 'A') ? '' : " AND ?:faq_questions.status IN ('A', 'H') ";
 
     // /**
     //  * Prepare params for question data SQL query
@@ -143,7 +143,7 @@ function fn_get_question_data($question_id, $lang_code = CART_LANGUAGE)
     //  */
     // fn_set_hook('get_question_data', $question_id, $lang_code, $fields, $joins, $condition);
 
-    $question = db_get_row("SELECT " . implode(", ", $fields) . " FROM ?:questions " . implode(" ", $joins) ." $condition");
+    $question = db_get_row("SELECT " . implode(", ", $fields) . " FROM ?:faq_questions " . implode(" ", $joins) ." $condition");
 
     // if (!empty($question)) {
     //     $question['main_pair'] = fn_get_image_pairs($question['question_image_id'], 'promo', 'M', true, false, $lang_code);
@@ -172,13 +172,13 @@ function fn_faq_page_update_question($data, $question_id, $lang_code = DESCR_SL)
     $data['localization'] = empty($data['localization']) ? '' : fn_implode_localizations($data['localization']);
 
     if (!empty($question_id)) {
-        db_query("UPDATE ?:questions SET ?u WHERE question_id = ?i", $data, $question_id);
-        db_query("UPDATE ?:question_descriptions SET ?u WHERE question_id = ?i AND lang_code = ?s", $data, $question_id, $lang_code);
+        db_query("UPDATE ?:faq_questions SET ?u WHERE question_id = ?i", $data, $question_id);
+        db_query("UPDATE ?:faq_question_descriptions SET ?u WHERE question_id = ?i AND lang_code = ?s", $data, $question_id, $lang_code);
     } else {
-        $question_id = $data['question_id'] = db_query("REPLACE INTO ?:questions ?e", $data);
+        $question_id = $data['question_id'] = db_query("REPLACE INTO ?:faq_questions ?e", $data);
 
         foreach (Languages::getAll() as $data['lang_code'] => $v) {
-            db_query("REPLACE INTO ?:question_descriptions ?e", $data);
+            db_query("REPLACE INTO ?:faq_question_descriptions ?e", $data);
         }
     }
     return $question_id;
