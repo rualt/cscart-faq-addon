0bb56ee1e8b399b51b0ab2ddd10f0b5d1cfb2490
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-30 18:50:02 +0400

    UNIT-7 : Get and display questions data on faq_page.view

--- app/addons/faq_page/controllers/frontend/faq_page.php
+++ app/addons/faq_page/controllers/frontend/faq_page.php
@@ -8,5 +8,11 @@ if (!defined('BOOTSTRAP')) {
 
 if ($mode == 'view') {
     $test = 'this is a view mode';
-    Tygh::$app['view']->assign('test', $test);
+
+    list($questions, $params) = fn_get_questions($_REQUEST, DESCR_SL, Registry::get('settings.Appearance.admin_elements_per_page'));
+    db_sort($params, $sortings, 'position', 'asc');
+    Tygh::$app['view']->assign([
+        'questions'  => $questions,
+        'search' => $params
+    ]);
 }
--- var/themes_repository/responsive/templates/addons/faq_page/views/faq_page/view.tpl
+++ var/themes_repository/responsive/templates/addons/faq_page/views/faq_page/view.tpl
@@ -1,2 +1,10 @@
-{$test}
-<p>hello</p>
+{include file="common/pagination.tpl"}
+
+{foreach from=$questions item=question}
+   {$question.question}
+   {$question.answer}
+{/foreach}
+
+{include file="common/pagination.tpl"}
+
+{capture name="mainbox_title"}{__("faq")}{/capture}
