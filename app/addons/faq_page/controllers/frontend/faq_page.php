7e7e09d901d6af3661967783323b31bf7cda7090
Ruslan Gazihanov <rualnar@gmail.com>
2020-03-31 16:21:17 +0400

    UNIT-7 : Remove $test variable and db_sort() call

--- app/addons/faq_page/controllers/frontend/faq_page.php
+++ app/addons/faq_page/controllers/frontend/faq_page.php
@@ -7,14 +7,13 @@ if (!defined('BOOTSTRAP')) {
 }
 
 if ($mode == 'view') {
-    $test = 'this is a view mode';
 
     list($questions, $params) = fn_get_questions(
         $_REQUEST,
         DESCR_SL,
         Registry::get('settings.Appearance.admin_elements_per_page')
     );
-    db_sort($params, $sortings, 'position', 'asc');
+
     Tygh::$app['view']->assign([
         'questions'  => $questions,
         'search' => $params
