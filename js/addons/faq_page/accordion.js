269eca969cf6a30d978cdb6a600ac74bc92732d0
Ruslan Gazihanov <rualnar@gmail.com>
2020-04-10 08:42:01 +0400

    UNIT-7.1 : Fix extra spaces and double quotes

--- js/addons/faq_page/accordion.js
+++ js/addons/faq_page/accordion.js
@@ -4,18 +4,18 @@
  *
  */
 (function (_, $) {
-    $( document ).ajaxComplete(function() {
+    $(document).ajaxComplete(function () {
         propellFaqAccordion();
     });
 
-    function propellFaqAccordion () {
-        $( ".faq-accordion" ).on( "click", function () {
-            if ($( 'h3' , this ).hasClass('ui-accordion-header-active ui-state-active')) {
-                $( 'h3', this ).removeClass( 'ui-accordion-header-active ui-state-active' );
-                $( '.ui-accordion-content', this ).hide( 100 );
+    function propellFaqAccordion() {
+        $('.faq-accordion').on('click', function () {
+            if ( $('h3', this).hasClass('ui-accordion-header-active ui-state-active') ) {
+                $('h3', this).removeClass('ui-accordion-header-active ui-state-active');
+                $('.ui-accordion-content', this).hide(100);
             } else {
-                $( 'h3', this ).addClass( 'ui-accordion-header-active ui-state-active' );
-                $( '.ui-accordion-content', this ).show( 200 );
+                $('h3', this).addClass('ui-accordion-header-active ui-state-active');
+                $('.ui-accordion-content', this).show(200);
             }
         });
     }
